ebb8b8c81b7576b3602ecb567520f92125f7f643
o6mopok <o6mopok@gmail.com>
2017-12-17 23:57:18 -0800

    some cleanup

--- test/specs/stateTaxTests/testStateTax.js
+++ test/specs/stateTaxTests/testStateTax.js
@@ -1,9 +1,37 @@
 var assert = require('assert');
+var WAIT_TIME = 3000
 var TEST_PAGE_URL = 'https://jungle-socks.herokuapp.com/';
 var EXPECTED_PAGE_TITLE = 'JungleSocks'
 var EXPECTED_PAGE_HEADER = 'Welcome To Jungle Socks!'
 
 var STATES_TO_TEST = ['CA','NY','MN']
+
+var ITEMS = [
+    {   
+        name:'zebra',
+        price:13
+    },
+    {   
+        name:'lion',
+        price:20
+    },
+    {   
+        name:'elephant',
+        price:35
+    },
+    {   
+        name:'giraffe',
+        price:17
+    }
+]
+
+function getSubtotal(items) {
+    var subtotal = 0
+    items.forEach(function(item) {
+        subtotal += parseFloat(item.price)
+    })
+    return subtotal
+}
 
 function getStateTax(state) {
     switch (state) {
@@ -40,20 +68,19 @@ function testTotalPriceByState(subtotal,state) {
     browser.url(TEST_PAGE_URL);
     browser.waitUntil(function () {
         return (browser.getUrl().indexOf(TEST_PAGE_URL)>-1)
-    },3000)
+    },WAIT_TIME)
 
-    var input = browser.element('[name="line_items[][quantity]"]');
-    input.setValue('1')
+    ITEMS.forEach(function(item){
+        browser.setValue('#line_item_quantity_'+item.name,1)        
+    })
     browser.selectByValue('select[name="state"]',state)
     browser.submitForm('form[action="/checkout/create"]');
     browser.waitUntil(function () {
         return (browser.getUrl().indexOf('/checkout/create')>0)
-    },3000)
+    },WAIT_TIME)
     var expectedSubtotal = getExpectedSubtotal(subtotal)
-    console.log("expectedSubtotal: ",expectedSubtotal)
     assert.equal(getFloatFromCurrency(browser.getText("#subtotal")), expectedSubtotal);  
     var calculatedExpectedTaxAmount = calculateExpectedTax(subtotal,state)
-    console.log('calculatedExpectedTaxAmount: '+calculatedExpectedTaxAmount)
     assert.equal(getFloatFromCurrency(browser.getText("#taxes")), calculatedExpectedTaxAmount);
     var calculatedExpectedTotal = calculateExpectedTotal(subtotal,state)
     assert.equal(getFloatFromCurrency(browser.getText("#total")), calculatedExpectedTotal);
@@ -66,8 +93,9 @@ before(function(){
 
 describe('JungleSocks Prices', function() {
     it('should calculate the right tax for states - ', function () {
+        var subtotal = getSubtotal(ITEMS)
         STATES_TO_TEST.forEach(function(state){
-            testTotalPriceByState(13.00,state)
+            testTotalPriceByState(subtotal,state)            
         })
     });
 });
