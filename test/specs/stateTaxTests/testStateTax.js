b57ef43cd0cb7be7ec27050360cf7a65485327ed
o6mopok <o6mopok@gmail.com>
2017-12-17 21:31:14 -0800

    test total

--- test/specs/stateTaxTests/testStateTax.js
+++ test/specs/stateTaxTests/testStateTax.js
@@ -16,8 +16,29 @@ describe('JungleSocks page', function() {
     it('should have the right title - '+EXPECTED_PAGE_HEADER, function () {
         var h1 = browser.getText('h1');
         assert.equal(h1, EXPECTED_PAGE_HEADER);
-    });    
+    });
+
+    describe('JungleSocks input form', function() {
+        browser.url(TEST_PAGE_URL);
+        it('should calculate the right total - ', function () {
+            var input = browser.element('[name="line_items[][quantity]"]');
+            input.setValue('1')
+            console.log('input', input)
+            assert.equal(input.getValue(), '1');
+            browser.selectByValue('select[name="state"]',"CA")
+            browser.submitForm('form[action="/checkout/create"]');
+            browser.waitUntil(function () {
+                return (browser.getUrl().indexOf('/checkout/create')>0)
+            },3000)
+            assert.equal(browser.getText("#subtotal"), '$13.00');            
+            assert.equal(browser.getText("#taxes"), '$1.04');            
+            assert.equal(browser.getText("#total"), '$14.04');            
+        });
+    });
+
 });
+
+
 
 after(function(){
     console.log('==== DONE!')
