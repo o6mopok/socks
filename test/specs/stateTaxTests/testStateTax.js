fb6e40d142da9d63280e21c7f125e1d2fc723927
o6mopok <o6mopok@gmail.com>
2017-12-18 14:37:10 -0800

    added negative testcases

--- test/specs/stateTaxTests/testStateTax.js
+++ test/specs/stateTaxTests/testStateTax.js
@@ -3,6 +3,7 @@ var WAIT_TIME = 10000
 var TEST_PAGE_URL = 'https://jungle-socks.herokuapp.com';
 var EXPECTED_PAGE_TITLE = 'JungleSocks'
 var EXPECTED_PAGE_HEADER = 'Welcome To Jungle Socks!'
+var EXPECTED_ERROR_MSG = 'We\'re sorry, but something went wrong (500)'
 
 var STATES_TO_TEST = require('./states')
 
@@ -42,6 +43,8 @@ function getStateTax(state) {
             return 0.06
         case 'MN':
             return 0.00
+        case 'NZ':
+            return -1.00
         default:
             return 0.05
     }
@@ -74,11 +77,11 @@ function testTotalPriceByState(subtotal,state) {
             browser.waitUntil(function () {
                 return (browser.getUrl().indexOf('/checkout/create')>0)
             },WAIT_TIME)
-            assert.equal(getFloatFromCurrency(browser.getText("#subtotal")), subtotal);  
+            assert.equal(getFloatFromCurrency(browser.getText("#subtotal")), subtotal, 'Subtotal incorrect');  
             var calculatedExpectedTaxAmount = calculateExpectedTax(subtotal,state)
-            assert.equal(getFloatFromCurrency(browser.getText("#taxes")), calculatedExpectedTaxAmount);
+            assert.equal(getFloatFromCurrency(browser.getText("#taxes")), calculatedExpectedTaxAmount, 'Tax calculated incorrectly');
             var calculatedExpectedTotal = calculateExpectedTotal(subtotal,state)
-            assert.equal(getFloatFromCurrency(browser.getText("#total")), calculatedExpectedTotal);
+            assert.equal(getFloatFromCurrency(browser.getText("#total")), calculatedExpectedTotal, 'Total incorrect');
 
         });
     });
@@ -94,6 +97,51 @@ describe('JungleSocks Prices for various states', function() {
         testTotalPriceByState(subtotal,state)            
     })
 })
+
+describe('JungleSocks Submit form with no state selected', function() {
+    var subtotal = getSubtotal(ITEMS)
+    it('should fail raising an error message', function () {    
+        browser.url(TEST_PAGE_URL);
+        browser.submitForm('form[action="/checkout/create"]');
+            browser.waitUntil(function () {
+                return (browser.getUrl().indexOf('/checkout/create')>0)
+            },WAIT_TIME)
+            assert.equal(browser.getTitle(), EXPECTED_ERROR_MSG);
+                
+    })
+})
+
+describe('JungleSocks Submit form with Quantity set to null', function() {
+    var subtotal = getSubtotal(ITEMS)
+    it('should fail raising an error message', function () {    
+        browser.url(TEST_PAGE_URL);
+        browser.selectByValue('select[name="state"]','CA')
+        browser.submitForm('form[action="/checkout/create"]');
+            browser.waitUntil(function () {
+                return (browser.getUrl().indexOf('/checkout/create')>0)
+            },WAIT_TIME)
+            assert.equal(browser.getTitle(), EXPECTED_ERROR_MSG, "Excpected error message : " +EXPECTED_ERROR_MSG+" didn't pop up");
+                
+    })
+})
+
+describe('JungleSocks Submit form with bad parameter passed to Quantity', function() {
+    var subtotal = getSubtotal(ITEMS)
+    it('should fail raising an error message', function () {    
+        browser.url(TEST_PAGE_URL);
+        ITEMS.forEach(function(item){
+            browser.setValue('#line_item_quantity_'+item.name, 'abc')        
+        })
+        browser.selectByValue('select[name="state"]','CA')
+        browser.submitForm('form[action="/checkout/create"]');
+            browser.waitUntil(function () {
+                return (browser.getUrl().indexOf('/checkout/create')>0)
+            },WAIT_TIME)
+            assert.equal(browser.getTitle(), EXPECTED_ERROR_MSG, "Excpected error message : " +EXPECTED_ERROR_MSG+" didn't pop up");
+                
+    })
+})
+
 
 after(function(){
     console.log('==== DONE!')
