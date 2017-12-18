e66e3f213ac5ecc1f151917cc721cdcdaecebd04
o6mopok <o6mopok@gmail.com>
2017-12-17 16:45:17 -0800

    fixed test case

--- test/specs/testStateTax.js
+++ test/specs/testStateTax.js
@@ -1,9 +1,9 @@
 var assert = require('assert');
 
-describe('webdriver.io page', function() {
-    it('should have the right title - the fancy generator way', function () {
+describe('JungleSocks page', function() {
+    it('should have the right title - JungleSocks', function () {
         browser.url('https://jungle-socks.herokuapp.com/');
         var title = browser.getTitle();
-        assert.equal(title, 'WebdriverIO - WebDriver bindings for Node.js');
+        assert.equal(title, 'JungleSocks');
     });
 });
